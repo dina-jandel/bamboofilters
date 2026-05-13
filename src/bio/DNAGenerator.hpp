edf250f881ade485eaa4209dd1454de9ba9fa8ae
Dina Janđel <dina.jandel@fer.hr>
2026-05-14 00:10:22 +0200

    Refactor DNA generator function signatures

--- src/bio/DNAGenerator.hpp
+++ src/bio/DNAGenerator.hpp
@@ -1,4 +1,9 @@
-#pragma once
+#ifndef DNA_GENERATOR_HPP
+#define DNA_GENERATOR_HPP
+
 #include <string>
 
-std::string generateDNA(size_t n);
+std::string generateDNA(size_t length);
+std::string generateRandomDNA(int k);
+
+#endif
