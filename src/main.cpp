15cbe8e8e6516e74088a30ea4faf6cf377699ad2
marija-spoljaric <marija.spoljaric@fer.hr>
2026-05-14 15:21:36 +0200

    Refactor experiment runner and improve dataset execution flow

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,34 @@
 #include <string>
 
 void runExperiments(
-    const std::string& outputFile,
-    const std::string& fastaFile = ""
-);
+    const std::string &outputFile,
+    const std::string &fastaFile = "");
 
 int main(
     int argc,
-    char* argv[]) {
+    char *argv[])
+{
 
     std::string fastaFile = "";
     std::string outputFile = "results.txt";
 
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         std::string arg = argv[i];
 
-        if (arg == "--file") {
+        if (arg == "--file")
+        {
             fastaFile = argv[++i];
         }
-        else if (arg == "--out") {
+        else if (arg == "--out")
+        {
             outputFile = argv[++i];
         }
     }
 
     runExperiments(
         outputFile,
-        fastaFile
-    );
+        fastaFile);
 
     std::cout
         << "Experiments completed.\n";
