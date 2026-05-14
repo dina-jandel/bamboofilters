5041805a96f50589553317a7e6e7e636942ed3f7
marija-spoljaric <marija.spoljaric@fer.hr>
2026-05-14 15:22:09 +0200

    Improve benchmarking pipeline for DNA filter experiments

--- src/utils/ExperimentRunner.cpp
+++ src/utils/ExperimentRunner.cpp
@@ -4,106 +4,117 @@
 #include <string>
 
 #include "../filter/BambooFilter.hpp"
+#include "../filter/BloomFilter.hpp"
 #include "../bio/DNAGenerator.hpp"
 #include "../bio/KMerGenerator.hpp"
 #include "Benchmark.hpp"
 #include "FASTAParser.hpp"
 
 void runExperiments(
-    const std::string& outputFile,
-    const std::string& fastaFile) {
-
+    const std::string &outputFile,
+    const std::string &fastaFile)
+{
     std::ofstream out(outputFile);
 
     std::vector<int> kValues = {10, 20, 50, 100, 200};
     std::vector<size_t> dnaSizes = {100000, 500000, 1000000};
 
-    for (size_t dnaSize : dnaSizes) {
-
+    for (size_t dnaSize : dnaSizes)
+    {
         std::string dna;
 
-        if (fastaFile.empty()) {
-            dna = generateRandomDNA(dnaSize);
+        if (fastaFile.empty())
+        {
+            dna = generateDNA(dnaSize);
             std::cout << "using synthetic dna\n";
-        } else {
+        }
+        else
+        {
             dna = readFasta(fastaFile);
             std::cout << "using fasta file\n";
         }
 
-        if (dna.empty()) continue;
+        if (dna.empty())
+            continue;
 
         std::cout << "dna size: " << dna.size() << "\n";
 
-        for (int k : kValues) {
-
-            if (k >= (int)dna.size()) continue;
+        for (int k : kValues)
+        {
+            if (k >= (int)dna.size())
+                continue;
 
             auto kmers = generateKmers(dna, k);
 
-            BambooFilter filter(20000);
+            BambooFilter bamboo(20000);
 
-            /* ---------------- INSERT ---------------- */
             Benchmark t1;
             t1.start();
+            for (const auto &kmer : kmers)
+                bamboo.insert(kmer);
+            long long bambooInsert = t1.stop();
 
-            for (const auto& kmer : kmers) {
-                filter.insert(kmer);
-            }
-
-            long long insertTime = t1.stop();
-
-            /* ---------------- LOOKUP (true positives) ---------------- */
             Benchmark t2;
             t2.start();
+            int bambooTP = 0;
+            for (const auto &kmer : kmers)
+                if (bamboo.contains(kmer))
+                    bambooTP++;
+            long long bambooLookup = t2.stop();
 
-            int truePositives = 0;
-
-            for (const auto& kmer : kmers) {
-                if (filter.contains(kmer)) {
-                    truePositives++;
-                }
-            }
-
-            long long lookupTime = t2.stop();
-
-            /* ---------------- FALSE POSITIVES ---------------- */
-            int falsePositives = 0;
+            int bambooFP = 0;
             int testSamples = 100000;
 
-            for (int i = 0; i < testSamples; i++) {
-
-                std::string fake = generateRandomDNA(k);
-
-                if (filter.contains(fake)) {
-                    falsePositives++;
-                }
+            for (int i = 0; i < testSamples; i++)
+            {
+                std::string fake = generateDNA(k);
+                if (bamboo.contains(fake))
+                    bambooFP++;
             }
 
-            double fpRate =
-                (double)falsePositives / testSamples * 100.0;
+            double bambooFPRate = (double)bambooFP / testSamples * 100.0;
 
-            double loadFactor =
-                filter.loadFactor();
+            BloomFilter bloom(20000);
 
+            Benchmark t3;
+            t3.start();
+            for (const auto &kmer : kmers)
+                bloom.insert(kmer);
+            long long bloomInsert = t3.stop();
 
+            Benchmark t4;
+            t4.start();
+            int bloomTP = 0;
+            for (const auto &kmer : kmers)
+                if (bloom.contains(kmer))
+                    bloomTP++;
+            long long bloomLookup = t4.stop();
 
+            int bloomFP = 0;
+            for (int i = 0; i < testSamples; i++)
+            {
+                std::string fake = generateDNA(k);
+                if (bloom.contains(fake))
+                    bloomFP++;
+            }
 
-            
+            double bloomFPRate = (double)bloomFP / testSamples * 100.0;
 
-            /* ---------------- OUTPUT ---------------- */
             out << "DNA size: " << dna.size() << "\n";
             out << "k: " << k << "\n";
-            out << "kmers: " << kmers.size() << "\n";
-            out << "insert time: " << insertTime << " ms\n";
-            out << "lookup time: " << lookupTime << " ms\n";
-            out << "load factor: " << loadFactor << "\n";
-            out << "true positives: " << truePositives << "\n";
-            out << "false positives: " << falsePositives << "\n";
-            out << "false positive rate: " << fpRate << "%\n\n";
+            out << "kmers: " << kmers.size() << "\n\n";
 
-            std::cout << "done k=" << k
-                      << " dna=" << dna.size()
-                      << "\n";
+            out << "--- BAMBOO FILTER ---\n";
+            out << "insert time [ms]: " << bambooInsert << "\n";
+            out << "lookup time [ms]: " << bambooLookup << "\n";
+            out << "false positive rate [%]: " << bambooFPRate << "\n\n";
+
+            out << "--- BLOOM FILTER ---\n";
+            out << "insert time [ms]: " << bloomInsert << "\n";
+            out << "lookup time [ms]: " << bloomLookup << "\n";
+            out << "false positive rate [%]: " << bloomFPRate << "\n\n";
+
+            std::cout << "done k=" << k << " dna=" << dna.size() << "\n";
         }
     }
 
