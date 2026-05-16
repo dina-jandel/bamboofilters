3c9c6ee697e685b7b1c3c9e3394610a4a7b93df6
Dina Janđel <dina.jandel@fer.hr>
2026-05-16 17:23:56 +0200

    komentari

--- src/bio/KMerGenerator.cpp
+++ src/bio/KMerGenerator.cpp
@@ -1,11 +1,29 @@
-#include "KMerGenerator.hpp"
+// ovaj kod napisala clanica tima: Marija Špoljarić
+#include "KMerGenerator.hpp" 
 
+
+// (k-mer: podniz DNA sekvence duljine k)
+/* pr.:
+    DNA sekvenca: ACGTAG
+    k = 3
+
+    k-meri:
+    ACG
+    CGT
+    GTA
+    TAG
+*/
+
+// fja koja generira sve moguce k-mere za neku DNA sekvencu 
 std::vector<std::string> generateKmers(const std::string &dna, int k)
 {
+    // vektor stringova koji ce spremati sve generirane k-mere
     std::vector<std::string> kmers;
 
     for (size_t i = 0; i + k <= dna.size(); i++)
     {
+        // uzima podstring koji pocinje na indeksu i i ima duljinu k znakova
+        // dodaje taj k-mer u vektor
         kmers.push_back(dna.substr(i, k));
     }
 
