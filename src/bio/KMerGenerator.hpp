6e8432f9f12eef29f60c92c0af567b968db9c31d
Dina Janđel <dina.jandel@fer.hr>
2026-05-16 17:27:07 +0200

    komentari
    Added a comment indicating the author of the code.

--- src/bio/KMerGenerator.hpp
+++ src/bio/KMerGenerator.hpp
@@ -1,5 +1,7 @@
+// ovaj kod napisala je clanica tima: Marija Špoljarić
 #pragma once
 #include <vector>
 #include <string>
 
+//deklaracija funkcije za generiranje svih k-mera neke DNA sekvence
 std::vector<std::string> generateKmers(const std::string &dna, int k);
