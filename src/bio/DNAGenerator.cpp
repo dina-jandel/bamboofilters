a7e85ec06ef2ac240cc27610f7e18092a76d3af5
Dina Janđel <dina.jandel@fer.hr>
2026-05-16 17:14:47 +0200

    komentari

--- src/bio/DNAGenerator.cpp
+++ src/bio/DNAGenerator.cpp
@@ -1,37 +1,40 @@
-#include "DNAGenerator.hpp"
-#include <random>
-
+// ovaj kod napisala članica tima: Marija Špoljarić
+#include "DNAGenerator.hpp" // header s deklaracijama funkcija
+#include <random> 
 #include <string>
-#include <cstdlib>
+#include <cstdlib> // za fju rand()
 
-std::string generateRandomDNA(int k) {
-    static const char bases[4] = {'A', 'C', 'G', 'T'};
 
-    std::string result;
-    result.reserve(k);
+std::string generateRandomDNA(int k) { // fja generira nasumicnu DNA sekvencu duljine k 
+    static const char bases[4] = {'A', 'C', 'G', 'T'};  // baze adenin citozin gvanin timin
 
-    for (int i = 0; i < k; i++) {
-        result.push_back(bases[rand() % 4]);
+    std::string result; // string za pohranu gen. dna sekvence
+    result.reserve(k); // unaprijed rezervira memoriju za k znakova, poboljsava performanse
+
+    for (int i = 0; i < k; i++) {  // k puta generira (nasumicno) jednu od baza i pohranjuje u string result
+        result.push_back(bases[rand() % 4]);   //rand()%4 daje broj 0, 1, 2 ili 3 (index polja u kojem su pohranjene baze)
     }
 
-    return result;
+    return result; // pov.vr. -> slucajno generirana DNA sekvenca
 }
 
 
-std::string generateDNA(size_t n)
-{
-    const char bases[4] = {'A', 'C', 'G', 'T'};
+std::string generateDNA(size_t n) {  // fja generira nasumicnu DNA sekvencu duljine n
+    const char bases[4] = {'A', 'C', 'G', 'T'}; // baze 
     std::string result;
-    result.reserve(n);
+    result.reserve(n); // unaprijed rezerv. za n znakova
 
+    //random_device generira seed za genrator slucajnih brojeva (seed sluzi za razlicite rezultate pri svakom pokretanju programa)
     std::random_device rd;
+    // Mersenne Twister geenrator sluc. brojeva
     std::mt19937 gen(rd());
+    // generator ima ravnomjeran odabir broja izmedu 0 i 3 (svaka baza ima jednaku vjerojatnost pojavljivanja)
     std::uniform_int_distribution<> dist(0, 3);
 
     for (size_t i = 0; i < n; i++)
-    {
-        result += bases[dist(gen)];
+    { 
+        result += bases[dist(gen)];   // dist(gen) -> generiranje broja 0, 1, 2 ili 3
     }
 
-    return result;
+    return result; // pov.vr. -> slucajno generirana DNA sekvenca
 }
